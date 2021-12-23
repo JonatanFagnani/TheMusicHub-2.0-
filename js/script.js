85b5dc0d7bf9586419d94a0ad54681dd1bd0797a
Jonatan Fagnani <jlfagnani@hotmail.com>
2021-12-23 15:31:53 -0300

    Player added

--- js/script.js
+++ js/script.js
@@ -147,3 +147,25 @@ hamburgerToggler.addEventListener("change", (e) => {
     ? (body.style.overflowY = "hidden")
     : (body.style.overflowY = "scroll");
 });
+
+//Reproductor
+
+const playCancion = document.getElementsByClassName("play");
+const stopCancion = document.getElementsByClassName("stop");
+const volumen = document.querySelector(".volumen");
+
+let audio;
+
+for (elemento of playCancion) {
+  elemento.addEventListener("click", function () {
+    let cancion = this.getAttribute("id");
+    audio = new Audio(`../assets/canciones/${cancion}.mp3`);
+    audio.play();
+  });
+}
+
+for (elemento of stopCancion) {
+  elemento.addEventListener("click", function () {
+    audio.pause();
+  });
+}
